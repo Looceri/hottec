96bc2baca1b8bd029ecf783d5d05cb284dd620c5
Loocer <142151699+Looceri@users.noreply.github.com>
2024-05-26 20:16:12 +0200

    register

--- src/pages/categoria/RegisterPage.vue
+++ src/pages/categoria/RegisterPage.vue
@@ -1,3 +1,46 @@
 <template>
-  register_category
+  <div>
+    <Container>
+      <input v-model="categoryName" type="text" placeholder="Enter category name" />
+      <button @click="registerCategory">Register Category</button>
+    </Container>
+  </div>
 </template>
+
+<script>
+import { initializeFirebase } from 'rc/boot/firebase'
+import Container from 'rc/components/ContainerLink.vue'
+
+export default {
+  components: { Container },
+  data () {
+    return {
+      categoryName: ''
+    }
+  },
+  methods: {
+    async registerCategory () {
+      try {
+        const { db } = await initializeFirebase()
+        const category = {
+          name: this.categoryName
+        }
+
+        const existingCategory = await db.collection('categories')
+          .where('name', '==', category.name)
+          .get()
+
+        if (existingCategory.empty) {
+          const docRef = await db.collection('categories').add(category)
+          console.log('Category registered with ID: ', docRef.id)
+          this.categoryName = ''
+        } else {
+          console.error('Category name already exists')
+        }
+      } catch (error) {
+        console.error('Error registering category: ', error)
+      }
+    }
+  }
+}
+</script>
